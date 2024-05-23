6dafe7578b96df3808af391e8c8127aaeb1f3924
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2024-05-23 10:13:45 +0300

    Update oop2.py

--- Exemple/oop2.py
+++ Exemple/oop2.py
@@ -1,25 +1,36 @@
 class Familie:
     def __init__(self, mama, tata, copiii):
-        self._mama = mama
+        # Inițializează membrii familiei
+        self._mama = mama  # Utilizează un atribut privat pentru mama
         self.tata = tata
         self.copiii = copiii
 
     @property
     def mama(self):
+        # Metodă getter pentru mama
         return self._mama
     
     @mama.setter
     def mama(self, value):
+        # Metodă setter pentru mama
         self._mama = value
 
     def informatii(self):
-        print(f'Familia mea este formata din: mama {self.mama}, tata {self.tata} si copiii {", ".join(self.copiii)}.')
+        # Metodă pentru a afișa informațiile despre familie
+        print(f'Familia mea este formată din: mama {self.mama}, tata {self.tata} și copiii {", ".join(self.copiii)}.')
 
 def main():
+    # Creează o instanță a clasei Familie
     familie1 = Familie("Maria", "Andrei", ["Mihai", "Adela"])
-    print(f'Pe mama mea o cheama {familie1.mama}')
+    # Afișează numele mamei
+    print(f'Pe mama mea o cheamă {familie1.mama}')
+    # Schimbă numele mamei
     familie1.mama = "Elena"
-    print(f'Acum pe mama mea o cheama {familie1.mama}')
+    # Afișează noul nume al mamei
+    print(f'Acum pe mama mea o cheamă {familie1.mama}')
+    # Afișează informațiile despre familie
     familie1.informatii()
 
+# Apelează funcția main pentru a executa codul
 main()
+
