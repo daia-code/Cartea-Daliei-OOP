3a0c879cf7cd9cef205753fbead65ef230372dd5
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2024-06-03 21:30:47 +0300

    Create exemple.py

---
+++ Sapt6/exemple.py
@@ -0,0 +1,20 @@
+import turtle
+
+# cream cursorul 
+
+creion = turtle.Turtle()
+
+# desenam o linie orizontala
+
+creion.forward(200)
+
+# cream fereastra , executam desenul si o tinem deschisa pentru a putea vedea rezultatul final
+
+turtle.mainloop()
+
+
+# folositi metoda left sau right pentru a roti directia cu 90 de grade si a desena linii in noua directie
+
+creion.left(90)
+
+creion.right(90)
