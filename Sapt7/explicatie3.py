3d4a86e8886201ab2c2d34455512af0a4276a87a
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2024-06-05 20:55:57 +0300

    Create explicatie3.py

---
+++ Sapt7/explicatie3.py
@@ -0,0 +1,34 @@
+'''
+CONCEPT : shape
+
+EXPLICAȚIE : Ne oferă posibilitatea să modificăm cursorul cu altă formă la alegere dintre:
+
+    sageată ('arrow' - default)
+
+    testoasă('turtle')
+
+    cerc ('circle')
+
+    patrat('square')
+
+    triunghi('triangle')
+
+    clasic('classic' - tot sageata)
+
+'''
+
+# EXEMPLU COD : 
+
+# incercati si alte forme ale cursorului
+
+import turtle
+
+creion = turtle.Turtle()
+
+creion.shape('turtle')
+
+for i in range(4):
+
+    creion.forward(100)
+
+    creion.left(90)
